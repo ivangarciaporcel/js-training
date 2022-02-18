39c6bba3aab411846c2df7378f7549227e240b69
Ivan Garcia <ivan.garcia@contractor.brightinsight.com>
2022-02-18 11:10:41 -0400

    Added components in separated files with correct imports and exports.

--- udacity/react-nanodegree/udacity-goals-todos/src/components/App.js
+++ udacity/react-nanodegree/udacity-goals-todos/src/components/App.js
@@ -1,7 +1,29 @@
-function App() {
-    return (
-        <div>Hello World</div>
-    );
+import React from "react";
+import {handleInitialData} from "../actions/share";
+import ConnectedTodos from "./Todos"
+import ConnectedGoals from "./Goals"
+import {connect} from "react-redux";
+
+class App extends React.Component {
+    componentDidMount() {
+        const {dispatch} = this.props
+        dispatch(handleInitialData())
+    }
+
+    render() {
+        if (this.props.loading === true) {
+            return <h3>Loading</h3>
+        }
+        return (
+            <div>
+                <ConnectedTodos/>
+                <ConnectedGoals/>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default connect(
+    (state) => ({
+        loading: state.loading
+    }))(App)
